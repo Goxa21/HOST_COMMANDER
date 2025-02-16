bd7b3ab06407e9972b2f9fb1085cd265324036c0
Goxa21 <goxa21@gmail.com>
2025-02-16 21:48:19 +0400

    Upd

--- HOST_COMMANDER_SIMPLE/ASSETS/FUnits/responces/responces.js
+++ HOST_COMMANDER_SIMPLE/ASSETS/FUnits/responces/responces.js
@@ -20,9 +20,9 @@ class ResponceCtrl {
                     { text: "Другое", custom_input: true },
                 ]
             },
-
         ];
         this.answer = {};
+        this.answer_button_base = null;
         this.INIT();
     }
 
@@ -37,9 +37,32 @@ class ResponceCtrl {
             THIS.responce_base.classList.remove('unraveled');
             THIS.r_close.classList.remove('unraveled');
         });
+        let human_name_input = document.querySelector('.human_name_input');
+        human_name_input.addEventListener('input', function (e) {
+            console.log(this.value);
+            console.log(human_name_input.value);
+            THIS.answer.human_name = human_name_input.value;
+            THIS.CHECK_SEND_ACCESS();
+        })
+
         for (let q of THIS.questions) {
             let q_base = THIS.DRAW_OPT_SELECTOR(q);
         }
+        THIS.answer_button_base = document.createElement('div');
+        THIS.answer_button_base.classList.add('ans_base');
+
+        let ans_button = document.createElement('div');
+        ans_button.classList.add('ans_button');
+        THIS.answer_button_base.classList.add('inactive');
+        ans_button.innerHTML = 'Отправить';
+        THIS.answer_button_base.appendChild(ans_button);
+        THIS.responce_base.appendChild(THIS.answer_button_base);
+        ans_button.addEventListener('click',function(e){
+            if (!THIS.answer_button_base.classList.contains('inactive')){
+                
+            }
+        })
+
     }
 
     DRAW_OPT_SELECTOR(cfg = {
@@ -59,7 +82,6 @@ class ResponceCtrl {
         THIS.responce_base.appendChild(opt_base);
 
         let opt_buttons = [];
-
         for (let opt of cfg.opts) {
             let opt_button = document.createElement('div');
             opt_button.innerHTML = opt.text;
@@ -69,6 +91,7 @@ class ResponceCtrl {
             opt_buttons.push(opt_button);
 
             if (opt.custom_input) {
+                opt_button.classList.add('custom_input');
                 let opt_input = document.createElement('input');
                 opt_input.placeholder = cfg.name;
                 opt_input.classList.add('r_opt_input');
@@ -76,6 +99,9 @@ class ResponceCtrl {
                 opt_input.addEventListener('input', function (e) {
                     THIS.answer[cfg.name] = opt_input.value;
                 });
+                opt_input.addEventListener('click', function (e) {
+                    e.stopPropagation();
+                })
             }
 
             opt_button.addEventListener('click', function (e) {
@@ -102,22 +128,21 @@ class ResponceCtrl {
                     opt_button.classList.add('selected');
 
                 }
+                THIS.CHECK_SEND_ACCESS();
                 console.log(THIS.answer);
             });
-            if (opt.custom_input) {
-
-
-            }
-            else {
-
-            }
-
         }
-        console.log(opt_base);
         return opt_base;
     }
 
-
+    CHECK_SEND_ACCESS(THIS = this) {
+        if (THIS.answer.human_name && THIS.answer["Вы подтвержаете своё присутствие?"]){
+            THIS.answer_button_base.classList.remove('inactive');
+        }
+        else {
+            THIS.answer_button_base.classList.add('inactive');
+        }
+    }
 }
 
 let responceCtrl = new ResponceCtrl();
